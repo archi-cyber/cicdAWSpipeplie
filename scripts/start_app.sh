475134749bccd557d2249651ce999a22c1dae8fd
junioir <junior.tefo01gmail.com>
2026-02-03 07:58:48 -0500

    changee adress

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["18.116.98.174"]/' /home/ubuntu/project_devops/agency/settings.py
+sed -i 's/\[]/\["54.146.205.142"]/' /home/ubuntu/project_devops/agency/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     
