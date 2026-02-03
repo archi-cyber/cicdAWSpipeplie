d2559cbd579efd9f929c6baec3f4b0c6a5374eae
junioir <junior.tefo01gmail.com>
2026-02-03 04:06:37 -0500

    firstPipeline

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["3.144.212.231"]/' /home/ubuntu/project_devops/agency/settings.py
+sed -i 's/\[]/\["18.116.98.174"]/' /home/ubuntu/project_devops/agency/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     
